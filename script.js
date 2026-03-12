37129b45fecc3e5def263521c857f225fe856e8d
mohamedS <salad2692@gmail.com>
2026-03-11 20:22:24 -0400

    added button functionality

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
     const mouseEvent = document.getElementById('mouse event');
     const textEvent = document.getElementById('text event');
     const formEvent = document.getElementById('form event');
+    const button1 = document.getElementById('Event1');
+    const button2 = document.getElementById('Event2');
+    const button3 = document.getElementById('Event3');
     mouseEvent.addEventListener('mouseenter', function() {
     console.log("mouse entered");
         });
@@ -15,6 +18,15 @@
                 event.preventDefault();
                 alert(`Submitted!`);
             });
+    button1.addEventListener('click', function() {
+            alert('Hi, this is button 1');
+        });
+    button2.addEventListener('click', function() {
+            alert('this is button 2');
+        });
+    button3.addEventListener('click', function() {
+            alert('three    ');
+        });
  })
  function unfocus() {
     console.log("Not in focus");
